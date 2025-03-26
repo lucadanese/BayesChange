706bfe8a5a1ee6c7890a3811c53797bc0b34e030
Luca Danese <46844267+lucadanese@users.noreply.github.com>
2025-03-26 16:16:41 +0100

    Update code.cpp

--- src/code.cpp
+++ src/code.cpp
@@ -3697,7 +3697,7 @@ Rcpp::List clust_cp_multi(arma::cube data,
 
   double num_groups_temp = max(partition_temp) + 1;
 
-  Rcpp::Rcout << "OK";s
+  Rcpp::Rcout << "OK";
 
   for(int i = 0; i < num_groups_temp; i++){
     orders_temp.row(i) = generate_random_order(data.slice(0).n_cols, 2.0/data.slice(0).n_cols, r).t();

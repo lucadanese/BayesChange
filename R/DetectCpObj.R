7871f002919492d78fb1cdd12e0bd3d712992c90
Luca Danese <danese.luca1@gmail.com>
2025-09-05 12:35:03 +0200

    fix plot

--- R/DetectCpObj.R
+++ R/DetectCpObj.R
@@ -384,7 +384,7 @@ plot.DetectCpObj <- function(x, y = NULL,
       est_cp = posterior_estimate(x, loss = loss, maxNClusters = maxNClusters,
                                   nRuns = nRuns, maxZealousAttempts = maxZealousAttempts)
 
-      data = t(data)
+      x$data = t(x$data)
 
       .df_sf_plot <- data.frame(as.vector(sapply(1:nrow(x$data), function(y) 1 - cumsum(x$data[y,]) / sum(x$data[y,]))),
                                 rep(1:ncol(x$data), nrow(x$data)),
@@ -527,7 +527,7 @@ plot.DetectCpObj <- function(x, y = NULL,
       est_cp = posterior_estimate(x, loss = loss, maxNClusters = maxNClusters,
                                   nRuns = nRuns, maxZealousAttempts = maxZealousAttempts)
 
-      data = t(data)
+      x$data = t(x$data)
 
       .df_sf_plot <- data.frame(as.vector(sapply(1:nrow(x$data), function(y) 1 - cumsum(x$data[y,]) / sum(x$data[y,]))),
                                 rep(1:ncol(x$data), nrow(x$data)),
